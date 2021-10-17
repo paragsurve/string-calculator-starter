76964093b1879c5de00d55775b7f26272debafa2
D8_49612_Parag <paraag.surve@gmail.com>
2021-10-17 20:47:39 +0530

    add method and test case created

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,9 +1,23 @@
 package calculator;
 
+import java.util.Arrays;
+
 class StringCalculator {
 
     public int add(String input) {
-        return 0;
+    	int result;
+    	String[] arr=input.split(",");
+    	if(input=="")
+    		return 0;
+    	if(arr.length==1) {
+    		result=Integer.parseInt(arr[0]);
+    		return result;
+    	}
+    	else if(arr.length==2) {
+    		result=Integer.parseInt(arr[0])+Integer.parseInt(arr[1]);
+    		return result;
+    	}
+		 return 0;
     }
 
 }
--- src/test/java/calculator/StringCalculatorShould.java
+++ src/test/java/calculator/StringCalculatorShould.java
@@ -17,4 +17,10 @@ class StringCalculatorShould {
         StringCalculator stringCalculator = new StringCalculator();
         assertEquals(1, stringCalculator.add("1"));
     }
+    
+    @Test
+    void string_with_two_numbers_should_return_sum_as_int() {
+    StringCalculator stringcalculator=new StringCalculator();
+    assertEquals(3, stringcalculator.add("1,2"));
+    }
 }
